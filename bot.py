5c5630ef2d809459259e4163b521b7ee6c75cb60
JianyueLab <valor@jianyuelab.cc>
2024-03-14 18:22:47 +0900

    修改成dict

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 import discord
 from discord.ext import commands
 from discord import app_commands
-from scripts import getAll, instance_control
+from scripts import instance_control, list_all, update
 
 TOKEN = os.getenv('TOKEN')
 conf_version = os.getenv('VERSION')
@@ -21,21 +21,19 @@ async def on_ready():
     # 终端输出
     try:
         synced = await client.tree.sync()
-        print(f"机器人已启动，已同步 {len(synced)} 条指令")
+        result = update()
+        print(f"机器人已启动，已同步 {len(synced)} 条指令。{result}")
+        
     except Exception as e:
         print(e)
-
+    
 # /list
-@client.tree.command(name='list', description='获取全部节点和实例')
-async def status(interaction: discord.Interaction, index: int):
+@client.tree.command(name='list', description='显示全部节点和实例')
+async def status(interaction: discord.Interaction):
     # 等待请求，并隐藏
     await interaction.response.defer(ephemeral=True)
-    result = getAll(index)
-    if result == 'None':
-        await interaction.followup.send("请求发生错误，请稍后重试")
-    else:
-        await interaction.followup.send(result)
-    return
+    result = list_all()
+    await interaction.followup.send(result)
     
 # /instance 
 @client.tree.command(name='instance', description='实例控制')
@@ -44,10 +42,10 @@ async def status(interaction: discord.Interaction, index: int):
     app_commands.Choice(name="关闭", value="stop"),
     app_commands.Choice(name="重启", value="restart"),
 ])
-async def instance(interaction: discord.Interaction, choices: app_commands.Choice[str], daemonid: str, instanceid: str):
+async def instance(interaction: discord.Interaction, choices: app_commands.Choice[str], instance_name: str):
     await interaction.response.defer(ephemeral=True)
     action = choices
-    result = instance_control(action, daemonid, instanceid)
+    result = instance_control(action, instance_name)
     await interaction.followup.send(result)
     return
 
--- scripts.py
+++ scripts.py
@@ -4,86 +4,163 @@ import requests
 URL = os.getenv('URL')
 APIKEY = os.getenv('APIKEY')
 
-def getAll(index):
-    response = requests.get(URL + '/api/service/remote_services?apikey=' + APIKEY)
-    data = response.json()
+def status_judge(status_data):
+    if status_data == -1:
+        status = '未知'
+    elif status_data == 0:
+        status = '关闭'
+    elif status_data == 1:
+        status = '正在关闭'
+    elif status_data == 2:
+        status = '正在启动'
+    elif status_data == 3:
+        status = '启动'
+    else:
+        status = '状态错误'
 
-    pre = []
-    
+    return status
+
+def request_judge(result_status):
     try:
-        if data["status"] == 200:
-            daemon_data = data['data'][index]
-            daemon_id = daemon_data['uuid']
-            daemon_nickname = daemon_data['remarks']
-            if daemon_id and daemon_nickname:
-                instance_data = daemon_data['instances']
-                daemon_result = (f'- **节点: ** {daemon_nickname}\n- **节点ID: ** {daemon_id} \n-----------------------------\n')
-                for instances in instance_data:
-                    instance_uuid = instances['instanceUuid']
-                    instance_nickname = instances['config']['nickname']
-                    instance_status_data = instances['status']
-                    if instance_status_data == -1:
-                        instance_status = '未知'
-                    elif instance_status_data == 0:
-                        instance_status = '关闭'
-                    elif instance_status_data == 1:
-                        instance_status = '正在关闭'
-                    elif instance_status_data == 2:
-                        instance_status = '正在启动'
-                    elif instance_status_data == 3:
-                        instance_status = '启动'
-                    else:
-                        instance_status = '返回错误'
-                    pre.append(f'- **实例：** {instance_nickname}\n- **实例ID: ** {instance_uuid}\n- **状态: ** {instance_status}')
-                result = daemon_result + '\n-----------------------------\n'.join(pre)
-                
-            else: 
-                result = '返回错误'
-            return result
-                
-        elif data["status"] == 400:
-            result = '请求函数错误'
-            return result
-        elif data["status"] == 403:
-            result = '权限不足'
-            return result
-        elif data["status"] == 500:
-            result = '服务器内部问题'
-            return result
-        else:
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f'发生错误：{e}')
-        return None
+        if result_status == 200:
+            judge_result = True
+        elif result_status == 400:
+            judge_result = '请求函数错误'
 
-def instance_control(action, daemonid, instanceid):
+        elif result_status == 403:
+            judge_result = '权限不足'
+
+        elif result_status == 500:
+            judge_result = '服务器内部问题'
+
+        else:
+            judge_result = '请求错误'
+    
+    except requests.exceptions.RequestException as e:
+        judge_result = '请求错误，检查日志予以获取帮助'
+
+    return judge_result
+
+def update():
+    global instance_nodes
+    # 获取json
+    response = requests.get(URL + '/api/service/remote_services?apikey=' + APIKEY)
+    # 分析json
+    original = response.json()
+    # 重置词典
+    instance_nodes = {}
+
+    result_status = original['status']
+    
+    # 调用函数判断返回
+    result_judge = request_judge(result_status)
+    
+    if result_judge == True:
+        for data in original['data']:
+            instances = data['instances']
+            for instance in instances:
+                nickname = instance['config']['nickname']
+                instance_uuid = instance['instanceUuid']
+                
+                status_data = instance['status']
+                
+                # 调用函数判断状态
+                status = status_judge(status_data)
+                    
+                daemon_uuid = data['uuid']
+                # 存入字典
+                instance_nodes[nickname] = {'uuid': instance_uuid, 'daemonId': daemon_uuid, 'status': status}
+
+                result = '成功获取实例列表'
+            
+    else:
+        result = result_judge
+        
+    return result
+
+def list_all():
+    # 获取json
+    response = requests.get(URL + '/api/service/remote_services?apikey=' + APIKEY)
+    # 分析json
+    original = response.json()
+    # 重置词典
+    instance_nodes = {}
+    
+    final = []
+
+    try:
+        if original["status"] == 200:
+            for data in original['data']:
+                instances = data['instances']
+                for instance in instances:
+                    nickname = instance['config']['nickname']
+                    instance_uuid = instance['instanceUuid']
+                    
+                    status_data = instance['status']
+                    
+                    status = status_judge(status_data)
+                        
+                    daemon_uuid = data['uuid']
+                    # 存入字典
+                    instance_nodes[nickname] = {'uuid': instance_uuid, 'daemonId': daemon_uuid, 'status': status}
+                    # 将昵称添加到 final 列表中
+                    final.append(f'**实例: ** {nickname}\n- **状态: ** {status}')
+
+                    result = '\n-------------------\n'.join(final)
+            
+        elif original["status"] == 400:
+            result = '请求函数错误'
+
+        elif original["status"] == 403:
+            result = '权限不足'
+
+        elif original["status"] == 500:
+            result = '服务器内部问题'
+
+        else:
+            result = '请求错误'
+    
+    except requests.exceptions.RequestException as e:
+        result = '请求错误，检查日志予以获取帮助'
+        
+    return result
+
+def instance_control(action, instance_name):
     action_value = action.value
     action_name = action.name
-    
-    print(action_value)
-    
-    if action_value == 'start':
-        response = requests.get(URL + '/api/protected_instance/open?apikey=' + APIKEY + '&uuid=' + instanceid + '&daemonId=' + daemonid)
-        data = response.json()
-    
-    if action_value == 'restart':
-        response = requests.get(URL + '/api/protected_instance/restart?apikey=' + APIKEY + '&uuid=' + instanceid + '&daemonId=' + daemonid)
-        data = response.json()
 
-    if action_value == 'stop':
-        response = requests.get(URL + '/api/protected_instance/kill?apikey=' + APIKEY + '&uuid=' + instanceid + '&daemonId=' + daemonid)
-        data = response.json()
+    target_name = instance_name
     
-    if data['status'] == 200:
-        result = f'正常{action_name}'
+    try: 
+        target_data = instance_nodes[target_name]
+        daemonid = target_data['daemonId']
+        instanceid = target_data['uuid']
         
-    elif data['status'] == 400:
-        result = '请求参数不正确'
+        if action_value == 'start':
+            response = requests.get(URL + '/api/protected_instance/open?apikey=' + APIKEY + '&uuid=' + instanceid + '&daemonId=' + daemonid)
+            data = response.json()
         
-    elif data['status'] == 403:
-        result = '无权限'
+        if action_value == 'restart':
+            response = requests.get(URL + '/api/protected_instance/restart?apikey=' + APIKEY + '&uuid=' + instanceid + '&daemonId=' + daemonid)
+            data = response.json()
+
+        if action_value == 'stop':
+            response = requests.get(URL + '/api/protected_instance/kill?apikey=' + APIKEY + '&uuid=' + instanceid + '&daemonId=' + daemonid)
+            data = response.json()
         
-    else:
-        result = '返回错误'
+        if data['status'] == 200:
+            result = f'正常 {action_name} **| 实例：** {target_name}'
+            
+        elif data['status'] == 400:
+            result = '请求参数不正确'
+            
+        elif data['status'] == 403:
+            result = '无权限'
+            
+        else:
+            result = '返回错误'
+            
+    except:
+        result = '命令错误'
 
     return result
